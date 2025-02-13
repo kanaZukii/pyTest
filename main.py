100867589f9d7ff45dcf47cc45154146219001d2
kanaZukii <cosmicpizza17@gmail.com>
2025-02-13 22:17:46 +0800

    3rd branch

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import testfile
 
 name = "stephen"
 
 print(name)
+testfile.maybe()
+
+# this is a test
---
+++ testfile.py
@@ -0,0 +1,5 @@
+def _holdmenow(stuff: str) -> None:
+    print(stuff)
+
+def maybe() -> None:
+    _holdmenow("Maybe six feet aint so far down")
